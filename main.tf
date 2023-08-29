f586de714a152933a82572c075d272822adb1776
EPAPRA <dave.devops109@gmail.com>
2023-08-29 18:22:24 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_mq_broker" "rabbitmq" {
   engine_version     = var.engine_version
   host_instance_type = var.host_instance_type
   security_groups    = [aws_security_group.rabbitmq.id]
-  subnet_ids         = var.deployment_mode == "SINGLE INSTANCE" ? [var.subnet_ids[0]] : var.subnet_ids
+  subnet_ids         = var.deployment_mode == "SINGLE_INSTANCE" ? [var.subnet_ids[0]] : var.subnet_ids
 
 #  configuration {
 #    id       = aws_mq_configuration.rabbitmq.id
