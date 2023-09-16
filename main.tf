5bb19d5ffddf3715a0b187a6ea0740de5d872028
EPAPRA <dave.devops109@gmail.com>
2023-09-16 23:49:18 +0530

    multibranch commit

--- main.tf
+++ main.tf
@@ -60,5 +60,5 @@ resource "aws_mq_broker" "rabbitmq" {
 resource "aws_ssm_parameter" "rabbitmq_endpoint" {
   name  = "${var.env}.rabbitmq.ENDPOINT"
   type  = "String"
-  value = replace(replace(aws_mq_broker.rabbitmq.instances.0.endpoints.0, "amp://",""), ":5671", "")
+  value = replace(replace(aws_mq_broker.rabbitmq.instances.0.endpoints.0, "amqps://",""), ":5671", "")
 }
